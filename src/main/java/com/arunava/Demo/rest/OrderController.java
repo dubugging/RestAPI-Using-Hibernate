b59051b3d6e832d537e3a367c2018b880be37ad9
Arunava Kirtan <kirtanarunava145@gmail.com>
2023-06-08 09:24:20 +0530

    Implemented Log4j

--- src/main/java/com/arunava/Demo/rest/OrderController.java
+++ src/main/java/com/arunava/Demo/rest/OrderController.java
@@ -1,8 +1,9 @@
 package com.arunava.Demo.rest;
 
 import com.arunava.Demo.Entity.Order;
-import com.arunava.Demo.dao.OrdersDAO;
 import com.arunava.Demo.serviceLayer.OrderService;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private static final Logger logger = Logger.getLogger(OrderController.class);
+    {
+        BasicConfigurator.configure();
+    }
     private final OrderService orderService;
     @Autowired
     public OrderController(OrderService os){
@@ -23,6 +28,7 @@ public class OrderController {
             @PathVariable int amount
     ){
         orderService.save(new Order(customerId, orderDate, amount));
+        logger.info("Inserted new row");
     }
     @PutMapping("/update/{id}/{amount}")
     public void updateAmount(
@@ -30,17 +36,21 @@ public class OrderController {
             @PathVariable int amount
     ){
         orderService.updateAmount(id, amount);
+        logger.info("Updated id="+id+" and amount is "+amount);
     }
     @DeleteMapping("/delete/{id}")
     public void deleteById(@PathVariable int id){
         orderService.deleteById(id);
+        logger.info("Deleted records of id="+id);
     }
     @GetMapping("/allOrders")
     public List<Order> getAllOrder(){
+        logger.info("Showing all records");
         return orderService.getAllOrders();
     }
     @GetMapping("/getOrder/{id}")
     public Order getOrderById(@PathVariable int id){
+        logger.info("Showing details of id="+id);
         return orderService.findById(id);
     }
 }
